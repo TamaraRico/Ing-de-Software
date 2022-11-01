9acc723de6504187cf99061c80c04e685441dc12
alondracv01 <71353627+alondracv01@users.noreply.github.com>
2022-11-01 02:02:56 -0700

    Se actualizaron las funciones

--- src/db/products.js
+++ src/db/products.js
@@ -1,7 +1,7 @@
 const objectId = requiere('mongodb').ObjectId
 
 //Agrega un solo producto
-const insertOneProduct = async (newListing) => {
+const insertOneProduct = async (products, newListing) => {
     try{
         const res = await products.insertOne(newListing);
         console.log(`New listing created with the following id: ${res.insertedId}`);
@@ -11,7 +11,7 @@ const insertOneProduct = async (newListing) => {
 }
 
 //Agrega varios productos
-const insertMultipleProducts = async (newListing) => {
+const insertMultipleProducts = async (products, newListings) => {
     try{
         const res = await products.insertMany(newListings);
         console.log(`${res.insertedCount} new listings created with the following id(s): `);
@@ -22,7 +22,7 @@ const insertMultipleProducts = async (newListing) => {
 }
 
 //Buscar un producto por nombre
-const findOneProductByName = async (MongoDB, nameOfProduct) => {
+const findOneProductByName = async (products, nameOfProduct) => {
     try{
         var database = MongoDB.getDB();
         var products = database.collection('products');
@@ -41,7 +41,7 @@ const findOneProductByName = async (MongoDB, nameOfProduct) => {
 }
 
 //Buscar varios producto por nombre
-const findMultipleProductsByName = async (nameOfProduct) => {
+const findMultipleProductsByName = async (products, nameOfProduct) => {
     try{
         const cursor = await products.find({name: nameOfProduct}).limit(50);
         const res = await cursor.toArray();
@@ -72,8 +72,42 @@ const findMultipleProductsByName = async (nameOfProduct) => {
     }
 }
 
+//Buscar todos los productos
+const findAllProducts = async (products) => {
+    try{
+        const cursor = await products.find().limit(50);  //Maximo 50 pero se necesita ver como tomar todos por lotes
+
+        const res = await cursor.toArray();
+
+        if(res.length > 0){
+            console.log(`Found listing(s) in the collection: `);
+            res.forEach((result, i) => {
+                console.log();
+                console.log(`${i + 1}. name: ${res.name}`);
+                console.log(`   category: ${res.category}`);
+                console.log(`   quantity: ${res.quantity}`);
+                console.log(`   priceUnit: ${res.priceUnit}`);
+                console.log(`   price: ${res.price}`);
+                console.log(`   manufacture: ${res.manufacture}`);
+                console.log(`   barcode: ${res.barcode}`);
+                console.log(`   lastPurchase: ${res.lastPurchase}`);
+                console.log(`   providerCode: ${res.providerCode}`);
+                console.log(`   discountPercent: ${res.discountPercent}`);
+                console.log(`   hasDiscount: ${res.hasDiscount}`);
+            })
+            
+            return res
+        } else{
+            console.log(`No listings found`);
+        }
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
 //Buscar un producto por codigo de barras
-const findOneProductByBarcode = async (productBarcode) => {
+const findOneProductByBarcode = async (products, productBarcode) => {
     try{
         const product = await products.findOne({barcode: productBarcode})
         if(product){
@@ -89,8 +123,33 @@ const findOneProductByBarcode = async (productBarcode) => {
     }
 }
 
+//Eliminar un solo producto
+const deleteOneProductByName = async (products, nameOfProduct) => {
+    try{
+        const res = await products.deleteOne({name: nameOfProduct});
+        console.log(`${res.deletedCount} document was deleted`);
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
+//Eliminar varios productos
+const deleteMultipleProductsByName = async (products, nameOfProduct) => {
+    try{
+        const res = await products.deleteMany({name: nameOfProduct});
+        console.log(`${res.deletedCount} document(s) was/were deleted`);
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
 module.exports = {insertOneProduct, 
-                  insertMultipleProducts, 
-                  findOneProductByName, 
-                  findMultipleProductsByName,
-                  findOneProductByBarcode}
+    insertMultipleProducts, 
+    findOneProductByName, 
+    findMultipleProductsByName,
+    findAllProducts,
+    findOneProductByBarcode,
+    deleteOneProductByName,
+    deleteMultipleProductsByName}
