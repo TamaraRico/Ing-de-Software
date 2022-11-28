9ae5be505c8c09f907d1e1e265b956f4b6406647
alondracv01 <71353627+alondracv01@users.noreply.github.com>
2022-11-28 14:48:51 -0800

    Tratando de mostrar la info en textfield

--- src/adminComponent/deleteProductComponent/deleteProduct.js
+++ src/adminComponent/deleteProductComponent/deleteProduct.js
@@ -9,23 +9,11 @@ import DialogTitle from "@mui/material/DialogTitle";
 import { Grid, InputAdornment, MenuItem } from "@mui/material";
 import Swal from "sweetalert2";
 
-const categories = [
-    {
-        value: 'Papeleria',
-        label: 'Papeleria',
-    },
-    {
-        value: 'Miscelanea',
-        label: 'Miscelanea',
-    },
-    {
-        value: 'Regalos',
-        label: 'Regalos',
-    }
-]
 export default function DeleteProduct() {
   const [open, setOpen] = React.useState(false);
-  const [category, setCategory] = React.useState('Papeleria');
+  const [name, setName] = useState('');
+  const [barcode, setBarcode] = useState('');
+
 
   const handleClickOpen = (event) => {
     event.preventDefault();
@@ -37,31 +25,27 @@ export default function DeleteProduct() {
     setOpen(false);
   };
 
-  const handleChangeCategory = (event) => {
+  const handleChangeBarcode = (event) => {
     event.preventDefault();
-    setCategory(event.target.value);
-  }
+    setBarcode(event.target.value);
+  };
+
+  const handleChangeName = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  };
 
   const handleSearchProduct = (event) => {
     event.preventDefault();
     
-    window.api.send('product:searchOne',window.document.getElementById('search').value);
+    window.api.send('product:getByBarcode',window.document.getElementById('search').value);
     //'product:getOne'
 
-    window.api.receive('product:get', (data) => {
+    window.api.receive('product:getOne', (data) => {
         const data2 = JSON.parse(data);
         if(data2 != null){
-            window.document.getElementById('barcode').value = data2.barcode;
-            window.document.getElementById('category').value = data2.category;
-            window.document.getElementById('name').value = data2.name;
-            window.document.getElementById('quantity').value = data2.quantity;
-            window.document.getElementById('priceUnit').value = data2.priceUnit;
-            window.document.getElementById('price').value = data2.price;
-            window.document.getElementById('manufacture').value = data2.manufacture;
-            window.document.getElementById('lastPurchase').value = data2.lastPurchase;
-            window.document.getElementById('providerCode').value = data2.providerCode;
-            window.document.getElementById('discountPercent').value = data2.discountPercent;
-            window.document.getElementById('hasDiscount').value = data2.hasDiscount;
+            setBarcode(data2.barcode);
+            setName(data2.name);
         } else{
             Swal.fire({
                 title: 'Error!',
@@ -116,46 +100,17 @@ export default function DeleteProduct() {
           <Grid container spacing={2}>
             <Grid item xs={12} md={6}>
                 <TextField fullWidth autoFocus margin="dense" id="search" label="Producto a buscar" type="text" variant="filled" />
+            </Grid>
+            <Grid item xs={12} md={6}>
                 <Button onClick={handleSearchProduct}>Buscar</Button>
             </Grid>
           </Grid>
           <Grid container spacing={2}>
             <Grid item xs={12} md={6}>
-              <TextField fullWidth autoFocus margin="dense" id="barcode" label="Codigo de barras" type="text" variant="filled" />
+              <TextField fullWidth autoFocus margin="dense" id="barcode" label="Codigo de barras" type="text" variant="filled" onChange={handleChangeBarcode} value={barcode} InputProps={{readOnly: true}} />
             </Grid>
             <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="name" label="Nombre" type="text" variant="filled" />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth select margin="dense" id="category" label="Categoria" variant="filled" value={category} onChange={handleChangeCategory}>{
-                categories.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="quantity" label="Cantidad en almacen" type="text" variant="filled" />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="price" label="Precio unitario" type="text" variant="filled" InputProps={{startAdornment: <InputAdornment position="start">$</InputAdornment>}}/>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="salePrice" label="Precio de venta" type="text" variant="filled" InputProps={{startAdornment: <InputAdornment position="start">$</InputAdornment>}}/>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="manufacture" label="Manufactura" type="text" variant="filled"/>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="lastPurchase" label="Ultimo dia de compra" type="text" variant="filled" />
-            </Grid>
-            <Grid item xs={12} md={12}>
-              <TextField fullWidth margin="dense" id="providerCode" label="Codigo de proveedor" type="text" variant="filled" />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="discountPercent" label="Porcentaje de descuento" type="text" variant="filled" />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth margin="dense" id="hasDiscount" label="¿Tiene descuento?" type="text" variant="filled" />
+              <TextField fullWidth margin="dense" id="name" label="Nombre" type="text" variant="filled" onChange={handleChangeName} value={name} InputProps={{readOnly: true}}/>
             </Grid>
           </Grid>
         </DialogContent>
